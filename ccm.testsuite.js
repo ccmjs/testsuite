2bc50179df570f18c810625b3d509e9a622c1410
Andre Kless <andre.kless@web.de>
2023-05-28 09:12:31 +0200

    update

--- ccm.testsuite.js
+++ ccm.testsuite.js
@@ -4,7 +4,8 @@
  * @overview ccmjs-based web component for unit testing
  * @author André Kless <andre.kless@web.de> 2023
  * @license The MIT License (MIT)
- * @version 4.0.0
+ * @version 4.2.0
+ * @domain https://ccmjs.github.io/testsuite/
  * @changes
  * - uses ccm.js v27.5.0 as default
  * - uses helper.mjs v8.4.2 as default
